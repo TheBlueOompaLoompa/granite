cca39d143c80cf794aa9a3825dddd9c7bbd9fc32
TheBlueOompaLoompa <blueoompaloompa72@gmail.com>
2022-10-14 17:02:00 -0500

    Create initial support for tab types properly

--- src/lib/tabs/Tab.svelte
+++ src/lib/tabs/Tab.svelte
@@ -1,6 +1,10 @@
 <script lang='ts'>
-  export let type: string;
+  import type { TabLocation } from '../types.js';
+  import Editor from './types/Editor.svelte';
+
+  export let type: TabLocation;
 </script>
 
 {#if type == 'editor'}
+<Editor/>
 {/if}
--- src/lib/tabs/Tabs.svelte
+++ src/lib/tabs/Tabs.svelte
@@ -1,9 +1,12 @@
 <script lang='ts'>
-  import type { ITab } from '../types.js';
+  import type { ITab, TabLocation } from '../types.js';
+  import Tab from './Tab.svelte';
+
   export let tabs: ITab[];
 </script>
 
 {#each tabs as tab}
-{#if !tab.popout}
+{#if tab.popout == TabLocation.Default}
+<Tab type={tab.type}/>
 {/if}
 {/each}
